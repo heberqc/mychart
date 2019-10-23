4fb38082a7dd3333943622bdbdd69b45bf6eb0e2
Heber Quequejana <heberqc@gmail.com>
2019-10-23 11:40:04 -0500

    add first chart

---
+++ src/App.svelte
@@ -0,0 +1,44 @@
+<script>
+	export let name
+	import { onMount } from 'svelte'
+	import Highcharts from 'highcharts'
+
+	onMount(async () => {
+		const myChart = Highcharts.chart('container', {
+			chart: {
+				type: 'bar',
+			},
+			title: {
+				text: 'Fruit Consumption',
+			},
+			xAxis: {
+				categories: ['Apples', 'Bananas', 'Oranges'],
+			},
+			yAxis: {
+				title: {
+					text: 'Fruit eaten',
+				},
+			},
+			series: [
+				{
+					name: 'Jane',
+					data: [1, 0, 4],
+				},
+				{
+					name: 'John',
+					data: [5, 7, 3],
+				},
+			],
+		})
+	})
+</script>
+
+<style>
+	h1 {
+		color: red;
+	}
+</style>
+
+<h1>Hello {name}!</h1>
+
+<div id="container" style="widht:100%;height:400px;"></div>
